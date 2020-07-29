3bfa3c5d4f875c99d19e7e4a1af9c5482d160778
JBrickhouse <jbrickhouse777@gmail.com>
2020-07-29 16:34:23 -0500

    Initial Calendar Line

--- script.js
+++ script.js
@@ -3,23 +3,24 @@
 // this function will only run once the DOM is ready for JavaScript code to execute.
 $(document).ready(function() {
     
-    // Displaying the current date and day of the week
-    var currentDay = moment().format('dddd, MMMM Do, YYYY');
-    $("#currentDay").html(currentDay);
+    // Displaying the current date and day of the week in the Jumbotron Header
+    var update = function () {
+        $("#currentDay").html(moment().format('dddd, MMMM Do, YYYY, h:mm:ss a'));
+    }
+    // This dynamically updates and displays the current time every second
+    setInterval(update, 1000);
 
 
 
+    // Set style classes on all the hour blocks
+    // .hour
 
+    // Set classes on all the text area time blocks
+    // If the time has passed, set the class to .past
+    // If the time is present, set the class to .present
+    // If the time is future, 
 
-
-
-
-
-
-
-
-
-
+    // 
 
 
 });
