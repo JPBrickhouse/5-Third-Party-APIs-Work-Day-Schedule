dd1cf67f3c6a608185fc3a8642ebba8d90ca36f7
JBrickhouse <jbrickhouse777@gmail.com>
2020-07-30 13:32:21 -0500

    localStorage breakthrough store and replace

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // ALL the code will go within this document.ready function
 // jQuery will detect this state of readiness so that code included inside
 // this function will only run once the DOM is ready for JavaScript code to execute.
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
     // Displaying the current date and day of the week in the Jumbotron Header
     var update = function () {
         $("#currentDay").html(moment().format('dddd, MMMM Do, YYYY, h:mm:ss a'));
@@ -12,81 +12,106 @@ $(document).ready(function() {
 
 
 
+    function submitRecall(textID) {
 
-    // Set classes on all the text area time blocks
-    // If the time has passed, set the class to .past
-    // If the time is present, set the class to .present
-    // If the time is future, set the class to .future
+        // Initializes an empty array
+        var calendarListCurrent = [];
 
+        // Run an if else statement, which checks to see if values exist in localStorage
+        // if calendarListStorage is null, then use the empty array initilized earlier
+        if (localStorage.getItem("calendarListStorage") === null) {
+            calendarListCurrent;
+        }
+        // else, calendarListStorage has values
+        // Therefore, use getItem to get the calendarListStorage array from localStorage
+        // and make that the calendarListCurrent array
+        else {
+            calendarListCurrent = JSON.parse(localStorage.getItem("calendarListStorage"));
+        }
 
+        // Checking to see if a particular textID already exists in the calendarListCurrent
+        let timeTestCheck = calendarListCurrent.find(calendarListCurrent => calendarListCurrent.time === textID);
 
+        // if timeTestCheck is true, meaning that a particular textID already exists,
+        // replace the value associated with that textID in the array of objects
+        if (timeTestCheck) {
 
-    // Set values?
+            // Using the map function to search and return the index position
+            // of an object whose property matches a search
+            // https://stackoverflow.com/questions/10557486/in-an-array-of-objects-fastest-way-to-find-the-index-of-an-object-whose-attribu
+            // Searching for the location that corresponds to the value of textID
+            var elementPos = calendarListCurrent.map(function (calendarListCurrent) { return calendarListCurrent.time; }).indexOf(textID);
+            
+            // Creating an object based on the contents
+            var eventReplace = {
+                time: textID,
+                events: document.getElementById(textID).value.trim()
+            };
 
-    // Set IDs on all the textareas
-    // nineAMtextarea, tenAMtextarea, etc.
-    // Set IDs on all of the buttons
-    // nineAMbutton, tenAMbutton, etc.
+            // Splicing the object into the array
+            // .splice requires three arguments
+            // elementPos refers to the index position of the object in the array
+            // 1 refers to the number of elements to be deleted
+            // (1 because we're only deleting that particular element)
+            // eventReplace is the element that is being inserted at that index
+            // https://www.javascripttutorial.net/javascript-array-splice/
+            calendarListCurrent.splice(elementPos,1,eventReplace);
+        }
 
-    // When the submit button is pushed, the text content is saved in localStorage
-    // It also changes the textarea value
+        // if timeTestCheck is false, meaning that a particular textID doesn't yet exist,
+        // create a new object associated with that textID and push it
+        // into the array of objects
+        else {
 
-    
+            // Creating an object based on the contents
+            var eventNew = {
+                time: textID,
+                events: document.getElementById(textID).value.trim()
+            };
+            // Pushing this object into the array
+            calendarListCurrent.push(eventNew);
+        }
 
-    function submitRecall(event) {
-        event.preventDefault();
-        
-        
-        // Check if it's blank
-        // If blank, just store it in localStorage
-        // Else, recall from local storage and display
-        // NEED to display initially, too
-
-        
+        // Set calendarListCurrent as the new version of calendarListStorage in localStorage
+        localStorage.setItem("calendarListStorage", JSON.stringify(calendarListCurrent));
     };
 
 
 
-    
-    
-    // Defining the function buttonClick, which defines what happens when
-    // the submit buttons are clicked...
-    function buttonClick () {
+    function buttonClick() {
+        // Console logging the id of the button that is clicked
         console.log(this.id);
-        
+
+        // Appending "text" to the end of the id of the button that is clicked
+        // And calling it a variable textID
         var textID = this.id + "text";
-        console.log(textID);
 
-
-        // UPDATE FROM HERE
-        // WRITE THE FUNCTION submitRecall
-        // CALL THE FUNCTION AND OUTPUT THE VALUE
-
-        document.getElementById(textID).value = "Store";
-
+        // Calling the submitReacll function and passing it textID
+        submitRecall(textID);
     }
-    $(".buttonButton").on("click",buttonClick);
+    // event listener assigned to every button with the class buttonButton
+    // that runs the function buttonClick when the buttno is clicked
+    $(".buttonButton").on("click", buttonClick);
 
 
 
-
-    function timeCheck () {
+    function timeCheck() {
         // Getting the current time
         var currentTime = moment().format()
 
         // Looping through each element with the class of timeText
-        $(".timeText").each(function(){
+        $(".timeText").each(function () {
 
             // Getting the text string from each p with class timeText
             var timeClassText = $(this).text();
-            
+
             // Parsing that text string and creating a moment object
-            var timeClassTime = moment(timeClassText,["h a"]);
+            var timeClassTime = moment(timeClassText, ["h a"]);
 
             // Adding an hour to that moment
             var timeClassTimePlusOne = moment(timeClassTime).add(1, "hour").format()
 
-            if (moment(currentTime).isBetween(timeClassTime,timeClassTimePlusOne)) {
+            if (moment(currentTime).isBetween(timeClassTime, timeClassTimePlusOne)) {
                 // if it's between, set class in the row as present
                 var overallRow = this.parentElement.parentElement;
                 overallRow.classList.remove("past", "present", "future");
